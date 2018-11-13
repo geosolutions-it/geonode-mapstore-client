175ce7cc4dac3ff4138f984d9763c5121c1dc49d
allyoucanmap <bovio.stefano@gmail.com>
2018-11-13 10:34:39 +0100

    Updated submodule and minor fixes on layout

--- geonode_mapstore_client/client/js/epics/index.js
+++ geonode_mapstore_client/client/js/epics/index.js
@@ -19,16 +19,8 @@ const {getSelectedLayer, layersSelector} = require("../../MapStore2/web/client/s
 const {mapSelector} = require("../../MapStore2/web/client/selectors/map");
 const ConfigUtils = require("../../MapStore2/web/client/utils/ConfigUtils");
 
-const {updateMapLayout} = require('../../MapStore2/web/client/actions/maplayout');
-const {TOGGLE_CONTROL, SET_CONTROL_PROPERTY} = require('../../MapStore2/web/client/actions/controls');
-const {MAP_CONFIG_LOADED} = require('../../MapStore2/web/client/actions/config');
-const {SIZE_CHANGE, CLOSE_FEATURE_GRID, OPEN_FEATURE_GRID} = require('../../MapStore2/web/client/actions/featuregrid');
-const {CLOSE_IDENTIFY, ERROR_FEATURE_INFO, TOGGLE_MAPINFO_STATE, LOAD_FEATURE_INFO, EXCEPTIONS_FEATURE_INFO} = require('../../MapStore2/web/client/actions/mapInfo');
-const {SHOW_SETTINGS, HIDE_SETTINGS} = require('../../MapStore2/web/client/actions/layers');
-const {PURGE_MAPINFO_RESULTS} = require('../../MapStore2/web/client/actions/mapInfo');
-const {mapInfoRequestsSelector} = require('../../MapStore2/web/client/selectors/mapinfo');
-const {head, get} = require('lodash');
-const {isFeatureGridOpen, getDockSize} = require('../../MapStore2/web/client/selectors/featuregrid');
+const {get} = require('lodash');
+const {updateMapLayoutEpic} = require('../../MapStore2/web/client/epics/maplayout');
 
 // const {basicError} = require('../../MapStore2/web/client/utils/NotificationUtils');
 /**
@@ -111,57 +103,6 @@ const _setThumbnail = (action$, {getState} = {}) =>
                     console.log(err);// eslint-disable-line
                 }
             });
-        });
-
-
-/**
- * Gets `MAP_CONFIG_LOADED`, `SIZE_CHANGE`, `PURGE_MAPINFO_RESULTS`, `CLOSE_FEATURE_GRID`, `OPEN_FEATURE_GRID`, `CLOSE_IDENTIFY`, `LOAD_FEATURE_INFO`, `TOGGLE_MAPINFO_STATE`, `TOGGLE_CONTROL`, `SET_CONTROL_PROPERTY` events.
- * Configures a map layout based on state of panels.
- * @param {external:Observable} action$ manages `MAP_CONFIG_LOADED`, `SIZE_CHANGE`, `PURGE_MAPINFO_RESULTS`, `CLOSE_FEATURE_GRID`, `OPEN_FEATURE_GRID`, `CLOSE_IDENTIFY`, `LOAD_FEATURE_INFO`, `TOGGLE_MAPINFO_STATE`, `TOGGLE_CONTROL`, `SET_CONTROL_PROPERTY`.
- * @memberof epics.mapLayout
- * @return {external:Observable} emitting {@link #actions.map.updateMapLayout} action
- */
-
-const updateMapLayoutEpic = (action$, store) =>
-    action$.ofType(MAP_CONFIG_LOADED, SIZE_CHANGE, CLOSE_FEATURE_GRID, OPEN_FEATURE_GRID, CLOSE_IDENTIFY, TOGGLE_MAPINFO_STATE, LOAD_FEATURE_INFO, EXCEPTIONS_FEATURE_INFO, TOGGLE_CONTROL, SET_CONTROL_PROPERTY, SHOW_SETTINGS, HIDE_SETTINGS, ERROR_FEATURE_INFO, PURGE_MAPINFO_RESULTS)
-        .switchMap(() => {
-
-            const state = store.getState();
-            const mapLayout = {left: {sm: 300, md: 500, lg: 600}, right: {md: 658}, bottom: {sm: 30}};
-            const leftPanels = head([
-                get(state, "controls.queryPanel.enabled") && {left: mapLayout.left.lg} || null,
-                get(state, "controls.widgetBuilder.enabled") && {left: mapLayout.left.md} || null,
-                get(state, "layers.settings.expanded") && {left: mapLayout.left.md} || null,
-                get(state, "controls.drawer.enabled") && {left: mapLayout.left.sm} || null
-            ].filter(panel => panel)) || {left: 0};
-
-            const rightPanels = head([
-                get(state, "controls.details.enabled") && {right: mapLayout.right.md} || null,
-                get(state, "controls.annotations.enabled") && {right: mapLayout.right.md} || null,
-                get(state, "controls.metadataexplorer.enabled") && {right: mapLayout.right.md} || null,
-                get(state, "mapInfo.enabled") && mapInfoRequestsSelector(state).length > 0 && {right: mapLayout.right.md} || null
-            ].filter(panel => panel)) || {right: 0};
-
-            const dockSize = getDockSize(state) * 100;
-            const bottom = isFeatureGridOpen(state) && {bottom: "calc(" + dockSize + '% + 30px)', dockSize} || {bottom: mapLayout.bottom.sm};
-
-            const transform = isFeatureGridOpen(state) && {transform: 'translate(0, -' + mapLayout.bottom.sm + 'px)'} || {transform: 'none'};
-            const height = {height: 'calc(100% - ' + mapLayout.bottom.sm + 'px)'};
-
-            const boundingMapRect = {
-                ...bottom,
-                ...leftPanels,
-                ...rightPanels
-            };
-
-            return Rx.Observable.of(updateMapLayout({
-                ...leftPanels,
-                ...rightPanels,
-                ...bottom,
-                ...transform,
-                ...height,
-                boundingMapRect
-            }));
         });
 
 module.exports = {
