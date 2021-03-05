27a8c2de2012f7ba5788a0ed853ca64875f83e9f
stefano bovio <stefano.bovio@geo-solutions.it>
2021-03-05 11:06:41 +0100

    Update MapStore submodule to include fix related to widget resizing (#83)
    * update mapstore submodule
    
    * update client bundle

--- geonode_mapstore_client/static/mapstore/dist/1555.d4fe9ae3be4e2f41d1a5.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgeonode_mapstore_client=self.webpackChunkgeonode_mapstore_client||[]).push([[1555],{31345:(e,o,p)=>{e.exports=p(61865)},61865:(e,o,p)=>{var l=p(29694)("flowRight",p(43808));l.placeholder=p(13002),e.exports=l},10295:(e,o,p)=>{var l=p(29694)("unset",p(73142));l.placeholder=p(13002),e.exports=l}}]);
