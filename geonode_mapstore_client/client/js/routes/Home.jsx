00f1c5fe0d7d695fa47751587a1765b41e51bc7a
Luca Orlandini <luca.orlandini@gmail.com>
2021-05-03 11:26:08 +0200

    Client - Flickering Search bar in homepage when the resource cards grid is empty or filtered #98 (#148)

--- geonode_mapstore_client/client/js/routes/Home.jsx
+++ geonode_mapstore_client/client/js/routes/Home.jsx
@@ -471,7 +471,6 @@ function Home({
                                 containerStyle={!isHeroVisible
                                     ? {
                                         marginTop: hideHero && dimensions.brandNavbarHeight,
-                                        minHeight: `calc(100vh - ${dimensions.brandNavbarHeight + dimensions.menuIndexNodeHeight + dimensions.footerNodeHeight}px )`,
                                         paddingBottom: dimensions.footerNodeHeight
                                     }
                                     : undefined}
