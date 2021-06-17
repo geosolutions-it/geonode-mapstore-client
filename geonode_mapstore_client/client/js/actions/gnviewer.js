a313c380415e515db1c64e4d9bb185bd5b896423
Muhwezi Deo <39124174+MuhweziDeo@users.noreply.github.com>
2021-06-17 18:57:52 +0300

    Client - Create new GeoStory page workflow  (#243)

--- geonode_mapstore_client/client/js/actions/gnviewer.js
+++ geonode_mapstore_client/client/js/actions/gnviewer.js
@@ -11,12 +11,19 @@ export const REQUEST_MAP_CONFIG = 'GEONODE_VIEWER:REQUEST_MAP_CONFIG';
 export const REQUEST_NEW_MAP_CONFIG = 'GEONODE_VIEWER:REQUEST_NEW_MAP_CONFIG';
 export const REQUEST_GEOSTORY_CONFIG = 'GEONODE_VIEWER:REQUEST_GEOSTORY_CONFIG';
 export const REQUEST_DOCUMENT_CONFIG = 'GEONODE_VIEWER:REQUEST_DOCUMENT_CONFIG';
+export const REQUEST_NEW_GEOSTORY_CONFIG = "GEONODE:VIEWER:REQUEST_NEW_GEOSTORY_CONFIG";
 
 
 export function requestLayerConfig(pk) {
     return {
         type: REQUEST_LAYER_CONFIG,
         pk
+    };
+}
+
+export function requestNewGeostoryConfig() {
+    return {
+        type: REQUEST_NEW_GEOSTORY_CONFIG
     };
 }
 
--- geonode_mapstore_client/client/js/epics/__tests__/gnviewer-test.js
+++ geonode_mapstore_client/client/js/epics/__tests__/gnviewer-test.js
@@ -1,10 +1,12 @@
 import expect from 'expect';
-import { gnViewerRequestNewMapConfig } from '@js/epics/gnviewer';
-
-import { requestNewMapConfig } from '@js/actions/gnviewer';
+import { gnViewerRequestNewGeoStoryConfig, gnViewerRequestNewMapConfig } from '@js/epics/gnviewer';
+import { requestNewGeostoryConfig, requestNewMapConfig  } from '@js/actions/gnviewer';
 import MockAdapter from 'axios-mock-adapter';
 import axios from '@mapstore/framework/libs/ajax';
+
 import { testEpic } from '@mapstore/framework/epics/__tests__/epicTestUtils';
+
+
 let mockAxios;
 
 describe("gnviewer epics", () => {
@@ -17,7 +19,43 @@ describe("gnviewer epics", () => {
         mockAxios.restore();
         setTimeout(done);
     });
+    it("should call notifications incase user does not have permission", (done) => {
+        mockAxios.onGet().reply(() => [200, {}]);
+        const NUM_ACTIONS = 2;
+        testEpic(
+            gnViewerRequestNewGeoStoryConfig,
+            NUM_ACTIONS,
+            requestNewGeostoryConfig(),
+            (actions) => {
+                try {
+                    expect(actions.map(({type}) => type )).toEqual(["GEOSTORY:SET_RESOURCE", "SHOW_NOTIFICATION"]);
+                    done();
+                } catch (error) {
+                    done(error);
+                }
+            },
+            {});
 
+    });
+
+    it("should call setNewResource, setResource, setResourceType, setGeoStoryResource when user has permissions", (done) => {
+        mockAxios.onGet().reply(() => [200, {}]);
+        const NUM_ACTIONS = 4;
+        testEpic(
+            gnViewerRequestNewGeoStoryConfig,
+            NUM_ACTIONS,
+            requestNewGeostoryConfig(),
+            (actions) => {
+                try {
+                    expect(actions.map(({type}) => type )).toEqual(["GEONODE:SET_NEW_RESOURCE", "GEONODE:SET_RESOURCE", "GEONODE:SET_RESOURCE_TYPE", "GEOSTORY:SET_RESOURCE"]);
+                    done();
+                } catch (error) {
+                    done(error);
+                }
+            },
+            {security: {user: {perms: ["add_resource"]}}});
+
+    });
 
     it("should call mapConfig", (done) => {
         mockAxios.onGet().reply(() => [200, {}]);
@@ -37,5 +75,4 @@ describe("gnviewer epics", () => {
             { security: { user: { perms: ["add_resource"] } } });
 
     });
-
 });
