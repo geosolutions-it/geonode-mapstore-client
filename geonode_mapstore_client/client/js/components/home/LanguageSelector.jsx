0682ae1abdff6f4c3d77f8fb7435f61c25839989
stefano bovio <stefano.bovio@geo-solutions.it>
2021-06-24 14:57:31 +0200

    Fix style for menu buttons (#262)

--- geonode_mapstore_client/client/js/components/home/LanguageSelector.jsx
+++ geonode_mapstore_client/client/js/components/home/LanguageSelector.jsx
@@ -37,11 +37,12 @@ const LanguageSelector = forwardRef(({
         >
             {inline
                 ? options.map((option) => {
+                    const active = option.value === value;
                     return (
                         <Button
-                            active={option.value === value}
+                            active={active}
                             key={option.key}
-                            variant="default"
+                            variant={active ? 'default' : 'primary'}
                             onClick={() => onSelect(option.value)}
                         >
                             {option.key}
