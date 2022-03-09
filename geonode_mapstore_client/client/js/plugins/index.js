42effa3be05f9628bf094e4b54c076a00109ee75
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-03-09 17:43:54 +0100

    Backport for 3.3.x of customized version for AIT (https://github.com/geosolutions-it/mapfish-print/issues/48)

--- geonode_mapstore_client/client/js/plugins/index.js
+++ geonode_mapstore_client/client/js/plugins/index.js
@@ -247,6 +247,26 @@ export const plugins = {
         'Print',
         () => import(/* webpackChunkName: 'plugins/print-plugin' */ '@mapstore/framework/plugins/Print')
     ),
+    PrintTextInputPlugin: toLazyPlugin(
+        'PrintTextInput',
+        () => import(/* webpackChunkName: 'plugins/print-text-input-plugin' */ '@mapstore/framework/plugins/print/TextInput')
+    ),
+    PrintOutputFormatPlugin: toLazyPlugin(
+        'PrintOutputFormat',
+        () => import(/* webpackChunkName: 'plugins/print-output-format-plugin' */ '@mapstore/framework/plugins/print/OutputFormat')
+    ),
+    PrintScalePlugin: toLazyPlugin(
+        'PrintScale',
+        () => import(/* webpackChunkName: 'plugins/print-scale-plugin' */ '@mapstore/framework/plugins/print/Scale')
+    ),
+    PrintProjectionPlugin: toLazyPlugin(
+        'PrintProjection',
+        () => import(/* webpackChunkName: 'plugins/print-projection-plugin' */ '@mapstore/framework/plugins/print/Projection')
+    ),
+    PrintGraticulePlugin: toLazyPlugin(
+        'PrintGraticule',
+        () => import(/* webpackChunkName: 'plugins/print-graticule-plugin' */ '@mapstore/framework/plugins/print/Graticule')
+    ),
     TimelinePlugin: toLazyPlugin(
         'Timeline',
         () => import(/* webpackChunkName: 'plugins/timeline-plugin' */ '@mapstore/framework/plugins/Timeline')
