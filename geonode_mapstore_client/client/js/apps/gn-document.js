36b30b9ac8da8f2105895396baeb7363f87fb6b8
David Quartey <42542676+DavidQuartz@users.noreply.github.com>
2022-06-24 07:37:04 +0000

    [Fixes #1053] Include base support for localization in gn-document app entry (#1054)

--- geonode_mapstore_client/client/js/apps/gn-document.js
+++ geonode_mapstore_client/client/js/apps/gn-document.js
@@ -5,21 +5,56 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import main from '@mapstore/framework/components/app/main';
+import ViewerRoute from '@js/routes/Viewer';
+import MainLoader from '@js/components/MainLoader';
+import Router, { withRoutes } from '@js/components/Router';
+import security from '@mapstore/framework/reducers/security';
+import gnresource from '@js/reducers/gnresource';
+import gnsettings from '@js/reducers/gnsettings';
+import { updateGeoNodeSettings } from '@js/actions/gnsettings';
 import {
     getEndpoints,
     getConfiguration,
-    getAccountInfo,
-    getDocumentByPk
+    getAccountInfo
 } from '@js/api/geonode/v2';
 import {
     setupConfiguration,
-    initializeApp
+    initializeApp,
+    getPluginsConfiguration,
+    storeEpicsCache
 } from '@js/utils/AppUtils';
-import MediaViewer from '@js/components/MediaViewer';
+import { ResourceTypes } from '@js/utils/ResourceUtils';
+import pluginsDefinition from '@js/plugins/index';
+import ReactSwipe from 'react-swipeable-views';
+import SwipeHeader from '@mapstore/framework/components/data/identify/SwipeHeader';
+import { requestResourceConfig } from '@js/actions/gnresource';
+import gnresourceEpics from '@js/epics/gnresource';
+const requires = {
+    ReactSwipe,
+    SwipeHeader
+};
+import '@js/observables/persistence';
 
 initializeApp();
+
+const DEFAULT_LOCALE = {};
+const ConnectedRouter = connect((state) => ({
+    locale: state?.locale || DEFAULT_LOCALE
+}))(Router);
+
+
+const routes = [{
+    name: 'document_embed',
+    path: [
+        '/'
+    ],
+    pageConfig: {
+        resourceType: ResourceTypes.DOCUMENT
+    },
+    component: ViewerRoute
+}];
 
 document.addEventListener('DOMContentLoaded', function() {
     getEndpoints().then(() => {
@@ -30,18 +65,63 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(([localConfig, user]) => {
                 setupConfiguration({ localConfig, user })
                     .then(({
+                        securityState,
+                        geoNodeConfiguration,
+                        pluginsConfigKey,
+                        configEpics,
+                        onStoreInit,
                         geoNodePageConfig,
-                        targetId = 'ms-container'
+                        targetId = 'ms-container',
+                        settings
                     }) => {
-                        getDocumentByPk(geoNodePageConfig.resourceId)
-                            .then((resource) => {
-                                ReactDOM.render(
-                                    <div className="gn-media-viewer">
-                                        <MediaViewer resource={resource}/>
-                                    </div>,
-                                    document.getElementById(targetId)
-                                );
-                            });
+
+                        const appEpics = {
+                            ...configEpics,
+                            ...gnresourceEpics
+                        };
+
+                        storeEpicsCache(appEpics);
+
+                        main({
+                            targetId,
+                            appComponent: withRoutes(routes)(ConnectedRouter),
+                            pluginsConfig: getPluginsConfiguration(localConfig.plugins, pluginsConfigKey),
+                            loaderComponent: MainLoader,
+                            lazyPlugins: pluginsDefinition.lazyPlugins,
+                            pluginsDef: {
+                                plugins: {
+                                    ...pluginsDefinition.plugins
+                                },
+                                requires: {
+                                    ...requires,
+                                    ...pluginsDefinition.requires
+                                }
+                            },
+                            initialState: {
+                                defaultState: {
+                                    ...securityState
+                                }
+                            },
+                            themeCfg: null,
+                            appReducers: {
+                                gnresource,
+                                gnsettings,
+                                security
+                            },
+                            appEpics,
+                            onStoreInit,
+                            geoNodeConfiguration,
+                            initialActions: [
+                                // add some settings in the global state to make them accessible in the monitor state
+                                // later we could use expression in localConfig
+                                updateGeoNodeSettings.bind(null, settings),
+                                ...(geoNodePageConfig.resourceId !== undefined
+                                    ? [requestResourceConfig.bind(null, ResourceTypes.DOCUMENT, geoNodePageConfig.resourceId, {
+                                        readOnly: geoNodePageConfig.isEmbed
+                                    })]
+                                    : [])
+                            ]
+                        });
                     });
             });
     });
