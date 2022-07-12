4547cdbe4ac4c8dcb77f904dac1246b6b00b511c
David Quartey <42542676+DavidQuartz@users.noreply.github.com>
2022-07-12 13:18:13 +0000

    [Fixes #1084] Use raw JSON body instead of ulencoded form for permissions PUT (#1086)

--- geonode_mapstore_client/client/js/api/geonode/v2/index.js
+++ geonode_mapstore_client/client/js/api/geonode/v2/index.js
@@ -728,7 +728,11 @@ export const getCompactPermissionsByPk = (pk) => {
 };
 
 export const updateCompactPermissionsByPk = (pk, body) => {
-    return axios.put(parseDevHostname(`${endpoints[RESOURCES]}/${pk}/permissions`), 'permissions=' + JSON.stringify(body))
+    return axios({
+        url: parseDevHostname(`${endpoints[RESOURCES]}/${pk}/permissions`),
+        data: body,
+        method: 'put'
+    })
         .then(({ data }) => data);
 };
 
@@ -751,7 +755,7 @@ export const downloadResource = (resource) => {
     return axios.get(url, {
         responseType: 'blob',
         headers: {
-            'Content_type': 'application/json'
+            'Content-Type': 'application/json'
         }
     })
         .then(({ data, headers }) => ({output: data, headers}));
